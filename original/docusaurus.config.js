7581c11f31277d63cde1f2d3f67f762569458dc0
wangzhx <1132154046@qq.com>
2023-02-22 18:23:00 +0800

    Rename: branch name

--- original/docusaurus.config.js
+++ original/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://zhuqiandai.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docusaurus/original/docs',
+  baseUrl: '/original',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.

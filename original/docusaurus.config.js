3c27783add6bfdaa7ac874314959716c25897705
wangzhx <1132154046@qq.com>
2023-02-22 18:10:58 +0800

    Fix: original config

--- original/docusaurus.config.js
+++ original/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://zhuqiandai.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/original/docs',
+  baseUrl: '/docusaurus/original/docs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
